c9dd71d9c67f56f6ee0042fbd8c6aa67b912b4e1
fauzanriff <Fauzanriff@gmail.com>
2016-02-05 22:02:37 +0700

    Add Bukalapak Mock Up Header

---
+++ js/function.js
@@ -0,0 +1,13 @@
+/* Function.js
+ * By Fauzan Muhammad Rifqy
+ *
+ * This file is use for Sign Up functionality which can pop up sign up form.
+ * 
+ */
+
+function called(){
+	window.alert("WELCOME BL USER");
+	return false;
+}
+
+document.getElementById("daftar").addEventListener("click", called);
